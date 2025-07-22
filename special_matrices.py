458dae3703c7dc290ee9c4ec1610a353513b0249
clinton-nyobe <cs.nyobe.ing@gmail.com>
2025-07-22 23:48:57 +0200

    SOLUTION : Gaussian Reduction

--- special_matrices.py
+++ special_matrices.py
@@ -61,18 +61,22 @@ def fixRowOne(A) :
 # Follow the instructions inside the function at each comment.
 def fixRowTwo(A) :
     # Insert code below to set the sub-diagonal elements of row two to zero (there are two of them).
-    
+    A[2] = A[2] - A[2,0]*A[0]
+    A[2] = A[2] - A[2,1]*A[1]
     
     # Next we'll test that the diagonal element is not zero.
-    if A[2,2] == 0 :
+    if A[2,2] == 0:
         # Insert code below that adds a lower row to row 2.
-        
+        A[2] = A[2] + A[3]
+
         # Now repeat your code which sets the sub-diagonal elements to zero.
-        
+        A[2] = A[2] - A[2,0]*A[0]
+        A[2] = A[2] - A[2,1]*A[1]
         
     if A[2,2] == 0 :
         raise MatrixIsSingular()
     # Finally set the diagonal element to one by dividing the whole row by that element.
+    A[2] = A[2] / A[2,2]
     
     return A
 
@@ -80,13 +84,40 @@ def fixRowTwo(A) :
 # Follow the instructions inside the function at each comment.
 def fixRowThree(A) :
     # Insert code below to set the sub-diagonal elements of row three to zero.
-    
-    
+    A[3] = A[3] - A[3,0]*A[0] 
+    A[3] = A[3] - A[3,1]*A[1]
+    A[3] = A[3] - A[3,2]*A[2]
     
     # Complete the if statement to test if the diagonal element is zero.
-    if :
+    if A[3,3]== 0:
         raise MatrixIsSingular()
-    # Transform the row to set the diagonal element to one.
     
+    # Transform the row to set the diagonal element to one.
+    A[3] = A[3] / A[3,3]
     return A
 
+if __name__ == "__main__":
+    A = np.array([
+        [2, 0, 0, 0],
+        [0, 3, 0, 0],
+        [0, 0, 4, 4],
+        [0, 0, 5, 5]
+    ], dtype=np.float_)
+print(isSingular(A))
+
+A = np.array([
+    [0, 7, -5, 3],
+    [2, 8, 0, 4],
+    [3, 12, 0, 5],
+    [1, 3, 1, 3]
+], dtype=np.float_)
+
+B = A.copy()
+fixRowZero(B)
+print("After Row 0:\n", B)
+fixRowOne(B)
+print("After Row 1:\n", B)
+fixRowTwo(B)
+print("After Row 2:\n", B)
+fixRowThree(B)
+print("After Row 3:\n", B)
